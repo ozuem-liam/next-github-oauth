215a372e815001d0fbeef992cdd8df7ea9e15694
ozuem-liam <ozuemdw@gmail.com>
2022-06-03 09:02:35 +0100

    first commit

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,7 +5,7 @@ export default NextAuth({
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLINT_SECRET,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
         })
     ]
 });
